4ef71b8611e4dc7ebf1144cfd091e39b3e263ff1
mayhun <mayhun28@gmail.com>
2025-07-25 17:20:11 +0900

    feat(jwt): 토큰 복호화를 위한 로직 추가

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
+from fastapi import HTTPException
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
@@ -9,8 +10,21 @@ if not SECRET_KEY:
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
+'''
+토큰 생성
+'''
 def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+'''
+토큰 복호화
+'''
+def decode_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=403, detail="유효하지 않은 토큰입니다.")
