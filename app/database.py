3ab01ed6363462ad9de8cc07c4fdfeb0491e1347
mayhun <mayhun28@gmail.com>
2025-08-26 18:50:53 +0900

    refactor: DB 세션을 AsyncSession으로 마이그레이션

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
+from typing import AsyncGenerator
 from dotenv import load_dotenv
 import os
 
@@ -12,16 +11,25 @@ host = os.getenv("DB_HOST")
 port = os.getenv("DB_PORT")    
 db = os.getenv("DB_NAME")      
 
-DB_URL = f'mysql+pymysql://{user}:{passwd}@{host}:{port}/{db}?charset=utf8'
+# asyncmy, aiomysql 둘 중 asyncmy가 빠르다고 하여 선택
+DB_URL = f'mysql+asyncmy://{user}:{passwd}@{host}:{port}/{db}?charset=utf8'
 
-engine = create_engine(DB_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
+# Async Engine 생성
+engine = create_async_engine(DB_URL, echo=True, future=True)
+
+# Async 세센팩토리
+SessionLocal = async_sessionmaker(
+    bind=engine,              # DB 연결 엔진
+    class_=AsyncSession,      # 비동기 세션 클래스 사용
+    autoflush=False,          # 자동 flush 비활성화 (직접 commit/flush)
+    autocommit=False,         # 자동 commit 비활성화 (명시적 commit 필요)
+    expire_on_commit=False    # commit 후에도 ORM 객체를 만료시키지 않음 (재사용 가능)
+)
+
+# Base 모델
 Base = declarative_base()
 
 # Dependency Injection 
-def get_db():
-    db = SessionLocal()
-    try : 
-        yield db
-    finally:
-        db.close()
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        yield session
